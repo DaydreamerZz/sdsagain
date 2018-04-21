fd38ad3f3156dcc455abfe0f5cceb2baf3a9a3dd
DaydreamerZz <zhzh402@163.com>
2018-04-21 14:51:40 +0800

    init sdsagain

---
+++ CMakeFiles/sds.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sds.dir/main.c.o"
+  "CMakeFiles/sds.dir/sds.c.o"
+  "CMakeFiles/sds.dir/zmalloc.c.o"
+  "sds.pdb"
+  "sds"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
